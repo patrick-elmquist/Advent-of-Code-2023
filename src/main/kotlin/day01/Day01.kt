541e40ba744c0790de566edda10e37b6c1531db5
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2023-12-01 11:31:32 +0100

    Cleanup

--- src/main/kotlin/day01/Day01.kt
+++ src/main/kotlin/day01/Day01.kt
@@ -9,21 +9,25 @@ fun main() {
     day(n = 1) {
         part1(expected = 55834) { input ->
             input.lines.sumOf { line ->
-                line.mapNotNull { it.digitToIntOrNull() }.let { it.first() * 10 + it.last() }
+                line.mapNotNull(Char::digitToIntOrNull).let { it.first() * 10 + it.last() }
             }
         }
         part1 test 1 expect 142
 
         part2(expected = 53221) { input ->
             val numberMap = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-                .flatMapIndexed { i, number -> listOf((i + 1).toString() to i + 1, number to i + 1) }.toMap()
+                .flatMapIndexed { i, number -> listOf((i + 1).toString() to i + 1, number to i + 1) }
+                .toMap()
 
             input.lines.sumOf { line ->
-                val first = numberMap.minBy { (it, _) -> line.indexOf(it).takeIf { it >= 0 } ?: Int.MAX_VALUE }
-                val last = numberMap.maxBy { (it, _) -> line.lastIndexOf(it).takeIf { it >= 0 } ?: Int.MIN_VALUE }
+                val first = numberMap.minBy { (it, _) -> line.indexOfOrNull(it) ?: Int.MAX_VALUE }
+                val last = numberMap.maxBy { (it, _) -> line.lastIndexOfOrNull(it) ?: Int.MIN_VALUE }
                 first.value * 10 + last.value
             }
         }
         part2 test 2 expect 281
     }
 }
+
+private fun String.indexOfOrNull(item: String) = indexOf(item).takeIf { it >= 0 }
+private fun String.lastIndexOfOrNull(item: String) = lastIndexOf(item).takeIf { it >= 0 }
