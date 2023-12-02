b8952a6a36649d6a330b85ad5a08cf177b136c4d
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2023-12-02 11:40:35 +0100

    Day 2 cleanup, better

--- src/main/kotlin/day02/Day02.kt
+++ src/main/kotlin/day02/Day02.kt
@@ -1,45 +1,34 @@
 package day02
 
+import common.Input
 import common.day
-import common.util.match
-import kotlin.math.max
 
 // answer #1: 2541
 // answer #2: 66016
 
 fun main() {
 
-    val pattern = """Game (\d+): (.*)""".toRegex()
-    fun parseIdAndRounds(line: String): Pair<Int, List<List<List<String>>>> {
-        return pattern.match(line) { (id, rounds) ->
-            id.toInt() to rounds.split("; ").map { it.split(", ").map { it.split(" ") } }
-        }
+    val entryPattern = """(\d+) (\w+)""".toRegex()
+    fun parseMapsOfMaxValues(input: Input) = input.lines.map {
+        entryPattern.findAll(it)
+            .map(MatchResult::destructured)
+            .groupBy { (_, color) -> color }
+            .mapValues { (_, vals) -> vals.maxOf { (count) -> count.toInt() } }
     }
 
     day(n = 2) {
         part1(expect = 2541) { input ->
             val available = mapOf("red" to 12, "green" to 13, "blue" to 14)
-            input.lines.sumOf { line ->
-                val (id, rounds) = parseIdAndRounds(line)
-                val areAllValid = rounds.all { round ->
-                    round.all { (count, color) -> count.toInt() <= available.getValue(color) }
-                }
-                id.takeIf { areAllValid } ?: 0
-            }
+            val mapsOfMaxValues = parseMapsOfMaxValues(input)
+            mapsOfMaxValues.withIndex()
+                .filter { (_, map) -> map.all { (color, count) -> count <= available.getValue(color) } }
+                .sumOf { (index, _) -> index + 1 }
         }
         part1 test 1 expect 8
 
         part2(expect = 66016) { input ->
-            input.lines.sumOf { line ->
-                val (_, rounds) = parseIdAndRounds(line)
-                val required = mutableMapOf<String, Int>()
-                rounds.forEach { round ->
-                    round.forEach { (count, color) ->
-                        required.merge(color, count.toInt(), ::max)
-                    }
-                }
-                required.values.reduce(Int::times)
-            }
+            val mapsOfMaxValues = parseMapsOfMaxValues(input)
+            mapsOfMaxValues.sumOf { it.values.reduce(Int::times) }
         }
         part2 test 1 expect 2286
     }
