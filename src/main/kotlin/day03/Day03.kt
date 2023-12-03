07fe083477b63da5c241510e3984d93f29a71727
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2023-12-03 09:09:29 +0100

    Day 3 cleanup

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -1,5 +1,6 @@
 package day03
 
+import common.Input
 import common.day
 import common.util.Point
 
@@ -8,45 +9,36 @@ import common.util.Point
 
 fun main() {
     day(n = 3) {
-        val pattern = """(\d+)""".toRegex()
         part1(expect = 532445) { input ->
-            val symbols = mutableMapOf<Point, Char>()
-            val parts = input.lines.flatMapIndexed { y, line ->
-                line.withIndex().filter { (_, c) -> !c.isDigit() && c != '.' }
-                    .forEach { (x, symbol) -> symbols[Point(x, y)] = symbol }
-                pattern.findAll(line).map {
-                    Part(it.value, Point(it.range.first, y))
-                }
-            }
-
-            parts.filter { part -> part.surrounding.any { symbols.containsKey(it) } }
-                .sumOf { it.value }
+            val (parts, symbols) = parsePartsAndSymbols(input)
+            parts.filter { part -> part.surrounding.any { symbols.containsKey(it) } }.sumOf { it.value }
         }
         part1 test 1 expect 4361
 
         part2(expect = 79842967) { input ->
-            val symbols = mutableMapOf<Point, Char>()
-            val parts = input.lines.flatMapIndexed { y, line ->
-                line.withIndex().filter { (_, c) -> !c.isDigit() && c != '.' }
-                    .forEach { (x, symbol) -> symbols[Point(x, y)] = symbol }
-                pattern.findAll(line).map {
-                    Part(it.value, Point(it.range.first, y))
-                }
-            }
-
+            val (parts, symbols) = parsePartsAndSymbols(input)
             symbols.filter { (_, symbol) -> symbol == '*' }
                 .keys
                 .sumOf { point ->
                     val valid = parts.filter { point in it.surrounding }
-                    if (valid.size == 2) {
-                        valid.map { it.value }.reduce(Int::times)
-                    } else {
-                        0
-                    }
+                    if (valid.size == 2) valid.map { it.value }.reduce(Int::times) else 0
                 }
         }
         part2 test 1 expect 467835
     }
+}
+
+private fun parsePartsAndSymbols(input: Input): Pair<List<Part>, MutableMap<Point, Char>> {
+    val pattern = """(\d+)""".toRegex()
+    val symbols = mutableMapOf<Point, Char>()
+    val parts = input.lines.flatMapIndexed { y, line ->
+        line.withIndex().filter { (_, c) -> !c.isDigit() && c != '.' }
+            .forEach { (x, symbol) -> symbols[Point(x, y)] = symbol }
+        pattern.findAll(line).map {
+            Part(it.value, Point(it.range.first, y))
+        }
+    }
+    return parts to symbols
 }
 
 private data class Part(val number: String, val point: Point) {
