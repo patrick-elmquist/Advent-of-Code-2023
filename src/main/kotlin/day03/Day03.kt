14ab879eb45164fd30a1091b9fa564292abd8ac0
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2023-12-03 12:01:01 +0100

    Day 3 cleanup

--- src/main/kotlin/day03/Day03.kt
+++ src/main/kotlin/day03/Day03.kt
@@ -11,7 +11,8 @@ fun main() {
     day(n = 3) {
         part1(expect = 532445) { input ->
             val (parts, symbols) = parsePartsAndSymbols(input)
-            parts.filter { part -> part.surrounding.any { symbols.containsKey(it) } }.sumOf { it.value }
+            parts.filter { part -> part.neighbors.any { symbols.containsKey(it) } }
+                .sumOf(Part::value)
         }
         part1 test 1 expect 4361
 
@@ -19,10 +20,9 @@ fun main() {
             val (parts, symbols) = parsePartsAndSymbols(input)
             symbols.filter { (_, symbol) -> symbol == '*' }
                 .keys
-                .sumOf { point ->
-                    val valid = parts.filter { point in it.surrounding }
-                    if (valid.size == 2) valid.map { it.value }.reduce(Int::times) else 0
-                }
+                .map { position -> parts.filter { part -> position in part.neighbors }.map(Part::value) }
+                .filter { partNumbers -> partNumbers.size == 2 }
+                .sumOf { partNumbers -> partNumbers.reduce(Int::times) }
         }
         part2 test 1 expect 467835
     }
@@ -30,20 +30,21 @@ fun main() {
 
 private fun parsePartsAndSymbols(input: Input): Pair<List<Part>, MutableMap<Point, Char>> {
     val pattern = """(\d+)""".toRegex()
+    val parts = mutableListOf<Part>()
     val symbols = mutableMapOf<Point, Char>()
-    val parts = input.lines.flatMapIndexed { y, line ->
-        line.withIndex().filter { (_, c) -> !c.isDigit() && c != '.' }
-            .forEach { (x, symbol) -> symbols[Point(x, y)] = symbol }
-        pattern.findAll(line).map {
-            Part(it.value, Point(it.range.first, y))
-        }
+    input.lines.forEachIndexed { y, line ->
+        symbols += line.withIndex()
+            .filter { (_, c) -> !c.isDigit() && c != '.' }
+            .map { (x, symbol) -> Point(x, y) to symbol }
+
+        parts += pattern.findAll(line).map { Part(it.value, Point(it.range.first, y)) }
     }
     return parts to symbols
 }
 
-private data class Part(val number: String, val point: Point) {
+private data class Part(private val number: String, private val point: Point) {
     val value = number.toInt()
-    val surrounding = buildSet {
+    val neighbors = buildSet {
         add(point.copy(x = point.x - 1))
         add(point.copy(x = point.x + number.length))
         for (x in (point.x - 1)..(point.x + number.length)) {
