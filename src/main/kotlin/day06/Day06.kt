9321551310efe462eb0401b2d1bafdc9eb18e516
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2023-12-06 06:24:30 +0100

    Day 6 Part 2 done

--- src/main/kotlin/day06/Day06.kt
+++ src/main/kotlin/day06/Day06.kt
@@ -1,20 +1,17 @@
 package day06
 
 import common.day
-import common.util.log
 
 // answer #1: 449820
-// answer #2:
+// answer #2: 42250895
 
 fun main() {
     day(n = 6) {
+        val pattern = """(\d+)""".toRegex()
+
         part1 { input ->
-            val pattern = """(\d+)""".toRegex()
             val (times, distances) = input.lines
                 .map { pattern.findAll(it).map(MatchResult::value).map(String::toInt).toList() }
-
-            times.log()
-            distances.log()
 
             times.zip(distances).map { (time, distance) ->
                 var hold = 0
@@ -34,11 +31,23 @@ fun main() {
         }
 
         part2 { input ->
+            val (time, distance) = input.lines
+                .map { pattern.findAll(it).map(MatchResult::value).joinToString("") }
+                .map(String::toLong)
 
+            var hold = 0L
+            var possible = 0L
+            while (hold < time) {
+                if (hold * (time - hold) > distance) {
+                    possible++
+                }
+                hold++
+            }
+            possible
         }
         verify {
-            expect result null
-            run test 1 expect Unit
+            expect result 42250895L
+            run test 1 expect 71503L
         }
     }
 }
