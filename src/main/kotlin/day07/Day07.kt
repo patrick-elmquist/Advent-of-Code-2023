4142cda1aeafa2bcedfcc2ee5cb198b87857358a
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2023-12-07 09:08:29 +0100

    Day 7 cleanup

--- src/main/kotlin/day07/Day07.kt
+++ src/main/kotlin/day07/Day07.kt
@@ -1,20 +1,24 @@
 package day07
 
-import ch.qos.logback.core.subst.Token.Type
 import common.day
-import common.util.log
 
 // answer #1: 251121738
 // answer #2: 251421071
 
+private const val JOKER = 1
+
+private const val FIVE_OF_A_KIND = 6
+private const val FOUR_OF_A_KIND = 5
+private const val FULL_HOUSE = 4
+private const val THREE_OF_A_KIND = 3
+private const val TWO_PAIR = 2
+private const val ONE_PAIR = 1
+private const val HIGH_CARD = 0
+
 fun main() {
     day(n = 7) {
         part1 { input ->
-            input.lines.map { parseLine(it) }
-                .sortedWith { o1, o2 -> compareHands(o1.first, o2.first) }
-                .reversed()
-                .withIndex()
-                .sumOf { (index, pair) -> (index + 1) * pair.second }
+            calculateWinnings(input.lines, useJokers = false)
         }
         verify {
             expect result 251121738L
@@ -22,11 +26,7 @@ fun main() {
         }
 
         part2 { input ->
-            input.lines.map { parseLine(it, withJokers = true) }
-                .sortedWith { o1, o2 -> compareHands(o1.first, o2.first, elevate = true) }
-                .reversed()
-                .withIndex()
-                .sumOf { (index, pair) -> (index + 1) * pair.second }
+            calculateWinnings(input.lines, useJokers = true)
         }
         verify {
             expect result 251421071L
@@ -35,115 +35,77 @@ fun main() {
     }
 }
 
-private const val FIVE = 0
-private const val FOUR = 1
-private const val FULL_HOUSE = 2
-private const val THREE = 3
-private const val TWO_PAIRS = 4
-private const val ONE_PAIR = 5
-private const val HIGH = 6
+private fun calculateWinnings(lines: List<String>, useJokers: Boolean): Long =
+    lines.asSequence()
+        .map { line -> parseCards(line, withJokers = useJokers) }
+        .sortedWith { o1, o2 -> compareHands(o1.first, o2.first, useJokers = useJokers) }
+        .map(Pair<List<Int>, Long>::second)
+        .withIndex()
+        .sumOf { (index, bid) -> (index + 1) * bid }
 
-private fun elevateType(type: Int, jokers: Int): Int {
-    if (jokers <= 0) return type
-    return when (type) {
-        FIVE -> FIVE
-        FOUR -> FIVE
-        FULL_HOUSE -> FIVE
-        THREE -> FOUR
-        TWO_PAIRS -> {
-            if (jokers == 2) {
-                FOUR
-            } else {
-                FULL_HOUSE
-            }
+private fun parseCards(line: String, withJokers: Boolean): Pair<List<Int>, Long> =
+    line.split(" ")
+        .let { (hand, bid) ->
+            hand.map { card -> cardToInt(card, withJokers) } to bid.toLong()
         }
 
-        ONE_PAIR -> THREE
-        HIGH -> ONE_PAIR
-        else -> error("type:$type jokers:$jokers")
-    }
-}
-
-private fun getType(card: List<Int>): Int {
-    val charSet = card.groupingBy { it }.eachCount()
-    val max = charSet.maxBy { it.value }
-    return when (charSet.size) {
-        1 -> FIVE // five
-        2 -> {
-            if (max.value == 4) {
-                FOUR // four
-            } else {
-                FULL_HOUSE // full
-            }
-        }
-
-        3 -> {
-            if (max.value == 3) {
-                THREE //three
-            } else {
-                TWO_PAIRS // two pairs
-            }
-        }
-
-        4 -> {
-            ONE_PAIR // one pair
-        }
-
-        else -> HIGH // high card
-    }
-}
-
-private fun compareHands(a: List<Int>, b: List<Int>, elevate: Boolean = false): Int {
-    val typeA = if (elevate) {
-        val jokersA = a.count { it == 1 }
-        elevateType(getType(a), jokersA)
-    } else {
-        getType(a)
-    }
-
-    val typeB = if (elevate) {
-        val jokersB = b.count { it == 1 }
-        elevateType(getType(b), jokersB)
-    } else {
-        getType(b)
-    }
-
-    return if (typeA != typeB) {
-        typeA.compareTo(typeB)
-    } else {
-        a.zip(b).firstOrNull { (a1, b1) -> a1 != b1 }?.let { (a1, b1) ->
-            b1.compareTo(a1)
-        } ?: 0
-    }
-}
-
-private fun parseLine(it: String, withJokers: Boolean = false): Pair<List<Int>, Long> =
-    it.split(" ").let { (hand, bid) -> hand.map { charToInt(it, withJokers) } to bid.toLong() }
-
-private fun intToChar(it: Int) = if (it in 0..9) {
-    it
-} else {
+private fun cardToInt(it: Char, withJokers: Boolean) =
     when (it) {
-        1 -> 'J'
-        10 -> 'T'
-        11 -> 'J'
-        12 -> 'Q'
-        13 -> 'K'
-        14 -> 'A'
-        else -> error("")
-    }
-}
-
-private fun charToInt(it: Char, withJokers: Boolean = false) = if (it.isDigit()) {
-    it.digitToInt()
-} else {
-    when (it) {
+        in '0'..'9' -> it.digitToInt()
         'T' -> 10
-        'J' -> if (withJokers) 1 else 11
+        'J' -> if (withJokers) JOKER else 11
         'Q' -> 12
         'K' -> 13
         'A' -> 14
         else -> error("")
     }
+
+private fun getType(cards: List<Int>, useJokers: Boolean): Int {
+    val cardSet = cards.groupingBy { it }.eachCount()
+    val countOfMostCommon = cardSet.maxOf { it.value }
+
+    val type = when (cardSet.size) {
+        1 -> FIVE_OF_A_KIND
+        2 -> if (countOfMostCommon == 4) FOUR_OF_A_KIND else FULL_HOUSE
+        3 -> if (countOfMostCommon == 3) THREE_OF_A_KIND else TWO_PAIR
+        4 -> ONE_PAIR
+        else -> HIGH_CARD
+    }
+
+    return if (useJokers) {
+        applyJokers(type, cards.count { it == JOKER })
+    } else {
+        type
+    }
 }
 
+private fun applyJokers(type: Int, jokers: Int): Int {
+    if (jokers <= 0) return type
+    return when (type) {
+        FIVE_OF_A_KIND -> FIVE_OF_A_KIND
+        FOUR_OF_A_KIND -> FIVE_OF_A_KIND
+        FULL_HOUSE -> FIVE_OF_A_KIND
+        THREE_OF_A_KIND -> FOUR_OF_A_KIND
+        TWO_PAIR -> {
+            if (jokers == 2) {
+                FOUR_OF_A_KIND
+            } else {
+                FULL_HOUSE
+            }
+        }
+        ONE_PAIR -> THREE_OF_A_KIND
+        HIGH_CARD -> ONE_PAIR
+        else -> error("type:$type jokers:$jokers")
+    }
+}
+
+private fun compareHands(a: List<Int>, b: List<Int>, useJokers: Boolean = false): Int {
+    val typeA = getType(a, useJokers)
+    val typeB = getType(b, useJokers)
+
+    return if (typeA != typeB) {
+        typeA.compareTo(typeB)
+    } else {
+        a.zip(b).firstOrNull { (a1, b1) -> a1 != b1 }?.let { (a1, b1) -> a1.compareTo(b1) } ?: 0
+    }
+}
