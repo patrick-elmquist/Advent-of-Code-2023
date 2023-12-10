77d2407e9cc6b403035ae5380aeb7926955942ee
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2023-12-10 22:24:10 +0100

    Cleanup

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -1,5 +1,6 @@
 package day10
 
+import common.Input
 import common.day
 import common.pointCharMap
 import common.util.Point
@@ -12,28 +13,19 @@ import kotlin.math.abs
 fun main() {
     day(n = 10) {
         part1 { input ->
-            val map = input.pointCharMap.filter { it.value != '.' }
-            val start = map.entries.single { it.value == 'S' }
-
-            val queue = mutableMapOf(start.key to 0)
-            val distances = mutableMapOf<Point, Int>()
-            distances[start.key] = 0
+            val (map, start) = parseMapAndStart(input)
+            val queue = mutableListOf(start)
+            val path = mutableMapOf(start to 0)
             while (queue.isNotEmpty()) {
-                val (current, dist) = queue.minBy { it.value }
+                val current = queue.minBy { path.getValue(it) }
+                val dist = path.getValue(current)
                 queue.remove(current)
 
-                distances[current] = dist
-
-                val (candidateA, candidateB) = getPathCandidates(map, current)
-
-                if (candidateA !in distances) {
-                    queue += candidateA to dist + 1
-                }
-                if (candidateB !in distances) {
-                    queue += candidateB to dist + 1
-                }
+                queue += getPathCandidates(map, current)
+                    .filter { it !in path }
+                    .onEach { path[it] = dist + 1 }
             }
-            distances.values.max()
+            path.values.max()
         }
         verify {
             expect result 6682
@@ -42,37 +34,31 @@ fun main() {
         }
 
         part2 { input ->
-            val map = input.pointCharMap.filter { it.value != '.' }
-            val start = map.entries.single { it.value == 'S' }
-
-            val queue = mutableMapOf(start.key to 0)
-            val path = mutableMapOf<Point, Int>()
-            path[start.key] = 0
+            val (map, start) = parseMapAndStart(input)
+            val queue = mutableListOf(start)
+            val path = mutableMapOf(start to 0)
             while (queue.isNotEmpty()) {
-                val (current, dist) = queue.minBy { it.value }
+                val current = queue.minBy { path.getValue(it) }
+                val dist = path.getValue(current)
                 queue.remove(current)
 
-                path[current] = dist
-
-                val neighbors = getPathCandidates(map, current)
-
-                if (map[current] == 'S') {
+                val candidates = getPathCandidates(map, current)
+                if (current == start) {
                     // This is what makes it take the correct path
-                    queue += neighbors.first to dist + 1
+                    val candidate = candidates.first()
+                    path[candidate] = dist + 1
+                    queue += candidate
                 } else {
-                    if (neighbors.first !in path) {
-                        queue += neighbors.first to dist + 1
-                    }
-                    if (neighbors.second !in path) {
-                        queue += neighbors.second to dist + 1
-                    }
+                    queue += candidates
+                        .filter { it !in path }
+                        .onEach { path[it] = dist + 1 }
                 }
             }
 
             val loop = path.keys.toList()
             // Pick's Theorem
-            // A = i + b/2 - 1
-            calculateAreaWithin(loop) - loop.size / 2 + 1
+            // A = i + b/2 - 1  <>  i = A - b/2 + 1
+            calculateGaussArea(loop) - loop.size / 2 + 1
         }
         verify {
             expect result 353
@@ -83,16 +69,22 @@ fun main() {
     }
 }
 
+private fun parseMapAndStart(input: Input): Pair<Map<Point, Char>, Point> {
+    val map = input.pointCharMap.filter { it.value != '.' }
+    val start = map.entries.single { it.value == 'S' }
+    return map to start.key
+}
+
 private fun getPathCandidates(
     map: Map<Point, Char>,
     origin: Point,
-): Pair<Point, Point> {
+): List<Point> {
     val (x, y) = origin
     return when (map[origin]) {
         '|' -> Point(x, y - 1) to Point(x, y + 1)
         '-' -> Point(x - 1, y) to Point(x + 1, y)
-        'L' -> Point(x, y = y - 1) to Point(x + 1, y)
-        'J' -> Point(x, y = y - 1) to Point(x - 1, y)
+        'L' -> Point(x, y - 1) to Point(x + 1, y)
+        'J' -> Point(x, y - 1) to Point(x - 1, y)
         '7' -> Point(x - 1, y) to Point(x, y + 1)
         'F' -> Point(x, y + 1) to Point(x + 1, y)
         'S' -> {
@@ -106,14 +98,13 @@ private fun getPathCandidates(
         }
 
         else -> error("")
-    }
+    }.toList()
 }
 
-private fun calculateAreaWithin(v: List<Point>): Int {
-    val n = v.size
+private fun calculateGaussArea(v: List<Point>): Int {
     var a = 0
-    for (i in 0 until n - 1) {
+    for (i in 0..<v.size - 1) {
         a += v[i].x * v[i + 1].y - v[i + 1].x * v[i].y
     }
-    return (abs(a + v[n - 1].x * v[0].y - v[0].x * v[n - 1].y) / 2f).toInt()
+    return abs(a + v.last().x * v.first().y - v.first().x * v.last().y) / 2
 }
