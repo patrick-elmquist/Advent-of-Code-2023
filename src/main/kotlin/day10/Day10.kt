3ee495ac0e1358cfdd05fec1a1fa487db7b24e15
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2023-12-10 20:17:38 +0100

    Somewhat better but still not great

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -22,13 +22,9 @@ fun main() {
                 val (current, dist) = queue.minBy { it.value }
                 queue.remove(current)
 
-                if (distances.getOrDefault(current, Int.MAX_VALUE) < dist) {
-                    continue
-                }
                 distances[current] = dist
 
-                val (x, y) = current
-                val (candidateA, candidateB) = getPathCandidates(map, current, y, x)
+                val (candidateA, candidateB) = getPathCandidates(map, current)
 
                 if (candidateA !in distances) {
                     queue += candidateA to dist + 1
@@ -50,34 +46,30 @@ fun main() {
             val start = map.entries.single { it.value == 'S' }
 
             val queue = mutableMapOf(start.key to 0)
-            val distances = mutableMapOf<Point, Int>()
-            distances[start.key] = 0
+            val path = mutableMapOf<Point, Int>()
+            path[start.key] = 0
             while (queue.isNotEmpty()) {
                 val (current, dist) = queue.minBy { it.value }
                 queue.remove(current)
 
-                if (distances.getOrDefault(current, Int.MAX_VALUE) < dist) {
-                    continue
-                }
-                distances[current] = dist
+                path[current] = dist
 
-                val (x, y) = current
-                val neighbors = getPathCandidates(map, current, y, x)
+                val neighbors = getPathCandidates(map, current)
 
                 if (map[current] == 'S') {
                     // This is what makes it take the correct path
                     queue += neighbors.first to dist + 1
                 } else {
-                    if (neighbors.first !in distances) {
+                    if (neighbors.first !in path) {
                         queue += neighbors.first to dist + 1
                     }
-                    if (neighbors.second !in distances) {
+                    if (neighbors.second !in path) {
                         queue += neighbors.second to dist + 1
                     }
                 }
             }
 
-            val loop = distances.keys.toList()
+            val loop = path.keys.toList()
             // Pick's Theorem
             // A = i + b/2 - 1
             calculateAreaWithin(loop) - loop.size / 2 + 1
@@ -93,27 +85,28 @@ fun main() {
 
 private fun getPathCandidates(
     map: Map<Point, Char>,
-    current: Point,
-    y: Int,
-    x: Int
-) = when (map[current]) {
-    '|' -> current.copy(y = y - 1) to current.copy(y = y + 1)
-    '-' -> current.copy(x = x - 1) to current.copy(x = x + 1)
-    'L' -> current.copy(y = y - 1) to current.copy(x = x + 1)
-    'J' -> current.copy(y = y - 1) to current.copy(x = x - 1)
-    '7' -> current.copy(x = x - 1) to current.copy(y = y + 1)
-    'F' -> current.copy(y = y + 1) to current.copy(x = x + 1)
-    'S' -> {
-        val (top, left, right, bottom) = current.neighbors().take(4).toList()
-        listOfNotNull(
-            left.takeIf { map[left] in listOf('-', 'L', 'F') },
-            bottom.takeIf { map[bottom] in listOf('|', 'L', 'J') },
-            right.takeIf { map[right] in listOf('-', 'J', '7') },
-            top.takeIf { map[top] in listOf('|', '7', 'F') },
-        ).let { (a, b) -> a to b }
-    }
+    origin: Point,
+): Pair<Point, Point> {
+    val (x, y) = origin
+    return when (map[origin]) {
+        '|' -> Point(x, y - 1) to Point(x, y + 1)
+        '-' -> Point(x - 1, y) to Point(x + 1, y)
+        'L' -> Point(x, y = y - 1) to Point(x + 1, y)
+        'J' -> Point(x, y = y - 1) to Point(x - 1, y)
+        '7' -> Point(x - 1, y) to Point(x, y + 1)
+        'F' -> Point(x, y + 1) to Point(x + 1, y)
+        'S' -> {
+            val (top, left, right, bottom) = origin.neighbors().toList()
+            listOfNotNull(
+                left.takeIf { map[left] in listOf('-', 'L', 'F') },
+                bottom.takeIf { map[bottom] in listOf('|', 'L', 'J') },
+                right.takeIf { map[right] in listOf('-', 'J', '7') },
+                top.takeIf { map[top] in listOf('|', '7', 'F') },
+            ).let { (a, b) -> a to b }
+        }
 
-    else -> error("")
+        else -> error("")
+    }
 }
 
 private fun calculateAreaWithin(v: List<Point>): Int {
@@ -122,5 +115,5 @@ private fun calculateAreaWithin(v: List<Point>): Int {
     for (i in 0 until n - 1) {
         a += v[i].x * v[i + 1].y - v[i + 1].x * v[i].y
     }
-    return (abs(a + v[n - 1].x * v[0].y - v[0].x * v[n -1].y) / 2f).toInt()
+    return (abs(a + v[n - 1].x * v[0].y - v[0].x * v[n - 1].y) / 2f).toInt()
 }
