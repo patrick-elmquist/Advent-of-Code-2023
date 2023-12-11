f043530391f126a48f1be6ba00adb96d79d3fbce
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2023-12-11 08:35:02 +0100

    Day 11 cleanup, can still be better

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -1,83 +1,68 @@
 package day11
 
+import common.Input
 import common.day
 import common.util.Point
-import common.util.log
 import kotlin.math.abs
 
 // answer #1: 10292708
-// answer #2:
+// answer #2: 790194712336
 
 fun main() {
     day(n = 11) {
         part1 { input ->
-            val galaxies = mutableListOf<Point>()
-            input.lines.forEachIndexed { y, row ->
-                row.forEachIndexed { x, c ->
-                    if (c == '#') {
-                        galaxies.add(Point(x, y))
-                    }
-                }
-            }
-            val minX = galaxies.minOf { it.x }
-            val maxX = galaxies.maxOf { it.x }
-            val minY = galaxies.minOf { it.y }
-            val maxY = galaxies.maxOf { it.y }
-
-            val emptyX = (minX..maxX) - galaxies.map { it.x }.toSet()
-            val emptyY = (minY..maxY) - galaxies.map { it.y }.toSet()
-
-            val moved = galaxies.map {
-                val (x, y) = it
-                val offsetX = emptyX.count { it < x }
-                val offsetY = emptyY.count { it < y }
-                Point(x + offsetX, y + offsetY)
-            }
-
-            emptyX.log()
-            emptyY.log()
-
-            val distances = mutableMapOf<Pair<Point, Point>, Int>()
-            moved.forEach { a ->
-                moved.forEach { b ->
-                    if (b == a) {
-
-                    } else {
-                        if (a.x == b.x) {
-                           if (a.y < b.y) {
-                               distances.put(a to b, a.distance(b))
-                           } else {
-                               distances.put(b to a, a.distance(b))
-                           }
-                        } else if (a.x < b.x) {
-                            distances.put(a to b, a.distance(b))
-                        } else {
-                            distances.put(b to a, a.distance(b))
-                        }
-                    }
-                }
-            }
-            distances.size.log("dist")
-
-            distances.values.sum()
+            calculateDistances(input, n = 1).sum()
         }
-
         verify {
-            expect result 10292708
-            run test 1 expect 374
+            expect result 10292708L
+            run test 1 expect 374L
         }
 
         part2 { input ->
-
+            calculateDistances(input, n = 1_000_000 - 1).sum()
         }
         verify {
-            expect result null
-            run test 1 expect Unit
+            expect result 790194712336L
         }
     }
 }
 
-private fun Point.distance(other: Point): Int {
-    return abs(other.x - x) + abs(other.y - y)
+private fun calculateDistances(input: Input, n: Int): List<Long> {
+    val galaxies = mutableListOf<Point>()
+    input.lines.forEachIndexed { y, row ->
+        row.forEachIndexed { x, c ->
+            if (c == '#') {
+                galaxies.add(Point(x, y))
+            }
+        }
+    }
+    val minX = galaxies.minOf { it.x }
+    val maxX = galaxies.maxOf { it.x }
+    val minY = galaxies.minOf { it.y }
+    val maxY = galaxies.maxOf { it.y }
+
+    val emptyX = (minX..maxX) - galaxies.map { it.x }.toSet()
+    val emptyY = (minY..maxY) - galaxies.map { it.y }.toSet()
+
+    val moved = galaxies.map { galaxy ->
+        val (x, y) = galaxy
+        val offsetX = emptyX.count { it < x } * n
+        val offsetY = emptyY.count { it < y } * n
+        Point(x + offsetX, y + offsetY)
+    }
+
+    val distances = mutableMapOf<Set<Point>, Long>()
+    moved.forEach { a ->
+        moved.forEach { b ->
+            if (b != a) {
+                distances[setOf(a, b)] = a.distance(b)
+            }
+        }
+    }
+
+    return distances.values.toList()
 }
 
+private fun Point.distance(other: Point): Long {
+    return abs(other.x.toLong() - x.toLong()) + abs(other.y.toLong() - y.toLong())
+}
