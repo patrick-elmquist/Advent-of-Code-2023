c347e91b46461362d1e95c2c235bb55cd0c53626
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2023-12-12 16:47:26 +0100

    Day 12 done, finally

--- src/main/kotlin/day12/Day12.kt
+++ src/main/kotlin/day12/Day12.kt
@@ -1,5 +1,6 @@
 package day12
 
+import common.Input
 import common.day
 
 // answer #1: 8180
@@ -8,11 +9,13 @@ import common.day
 fun main() {
     day(n = 12) {
         part1 { input ->
-            input.lines.sumOf { line ->
-                val (pattern, counts) = line.split(" ")
-                    .let { (a, b) -> a to b.split(",").map(String::toInt) }
-                countArrangementsImproved(pattern, amounts = counts)
-            }
+            parseInput(input)
+                .sumOf { (springs, groups) ->
+                    countCombinations(
+                        springs = springs,
+                        groups = groups
+                    )
+                }
         }
         verify {
             expect result 8180L
@@ -20,17 +23,13 @@ fun main() {
         }
 
         part2 { input ->
-            input.lines.sumOf { line ->
-                val (pattern, counts) = line.split(" ")
-                    .let { (a, b) -> a to b.split(",").map(String::toInt) }
-
-                val patternExpanded = generateSequence { pattern }
-                    .take(n = 5).joinToString("?")
-                val countsExpanded = generateSequence { counts }
-                    .take(n = 5).toList().flatten()
-
-                countArrangementsImproved(patternExpanded, countsExpanded)
-            }
+            parseInput(input)
+                .sumOf { (springs, group) ->
+                    countCombinations(
+                        springs = repeat5Times { springs }.joinToString("?"),
+                        groups = repeat5Times { group }.flatten(),
+                    )
+                }
         }
         verify {
             expect result 620189727003627L
@@ -39,45 +38,53 @@ fun main() {
     }
 }
 
-private fun countArrangementsImproved(
-    input: String,
-    amounts: List<Int>,
+private fun countCombinations(
+    springs: String,
+    groups: List<Int>,
     memo: MutableMap<Pair<String, List<Int>>, Long> = hashMapOf()
 ): Long {
-    if (amounts.isEmpty()) {
-        return if ('#' in input) 0 else 1
+    if (groups.isEmpty()) {
+        return if (springs.none { it == '#' }) 1 else 0
     }
 
-    if (input.isEmpty()) {
+    if (springs.isEmpty()) {
         return 0
     }
 
-    return memo.getOrPut(input to amounts) {
-        var total = 0L
-        val current = input.first()
-        val amount = amounts.first()
+    return memo.getOrPut(springs to groups) {
+        val current = springs.first()
+        val amount = groups.first()
 
+        var total = 0L
         if (current in ".?") {
-            total += countArrangementsImproved(
-                input = input.drop(1),
-                amounts = amounts,
+            total += countCombinations(
+                springs = springs.drop(1),
+                groups = groups,
                 memo = memo,
             )
         }
 
-        val hasEnoughSpaceLeft = amount <= input.length
-        if (current in "#?" && hasEnoughSpaceLeft) {
-            val hasNoSpacing = '.' !in input.take(amount)
-            val hasProperEnding = amount == input.length || input[amount] in ".?"
-            if (hasNoSpacing && hasProperEnding) {
-                total += countArrangementsImproved(
-                    input = input.drop(amount + 1),
-                    amounts = amounts.drop(1),
+        if (current in "#?" && amount <= springs.length) {
+            val hasNoDots = springs.take(amount).none { it == '.' }
+            val hasProperEnding = amount == springs.length || springs[amount] in ".?"
+
+            if (hasNoDots && hasProperEnding) {
+                total += countCombinations(
+                    springs = springs.drop(amount + 1),
+                    groups = groups.drop(1),
                     memo = memo,
                 )
             }
         }
 
         total
+    }
+}
+
+private fun <T> repeat5Times(block: () -> T): List<T> = generateSequence(block).take(5).toList()
+
+private fun parseInput(input: Input): List<Pair<String, List<Int>>> {
+    return input.lines.map {line ->
+        line.split(" ").let { (a, b) -> a to b.split(",").map(String::toInt) }
     }
 }
