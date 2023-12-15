d434a56ecb4bccfe8273ee8b4e517e9c2580282a
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2023-12-15 06:42:57 +0100

    Day 15 Cleanup

--- src/main/kotlin/day15/Day15.kt
+++ src/main/kotlin/day15/Day15.kt
@@ -1,18 +1,15 @@
 package day15
 
 import common.day
-import common.util.log
 import java.util.*
 
 // answer #1: 502139
-// answer #2:
+// answer #2: 284132
 
 fun main() {
     day(n = 15) {
         part1 { input ->
-            input.lines.first()
-                .split(",")
-                .sumOf { hash(it) }
+            input.lines.first().split(",").sumOf(::hash)
         }
         verify {
             expect result 502139
@@ -20,57 +17,41 @@ fun main() {
         }
 
         part2 { input ->
-            val boxes = List(256) { LinkedList<Pair<String, Int>>() }
-            input.lines.first().split(",").forEach { input ->
+            val boxes = List(256) { LinkedList<Pair<String, String>>() }
+            input.lines.first().split(",").forEach { step ->
                 when {
-                    '-' in input -> {
-                        val label = input.dropLast(1)
-                        val box = hash(label)
-                        val list = boxes[box]
-                        val index = list.indexOfFirst { it.first == label }
-                        if (index >= 0) {
-                            list.removeAt(index)
-                        }
+                    '-' in step -> {
+                        val label = step.dropLast(1)
+                        val content = boxes[hash(label)]
+                        val index = content.indexOfFirst { it.first == label }
+                        if (index >= 0) content.removeAt(index)
                     }
 
-                    '=' in input -> {
-                        val (label, focal) = input.split("=").let { (label, fl) -> label to fl.toInt() }
-                        val box = hash(label)
-                        val list = boxes[box]
-                        val index = list.indexOfFirst { it.first == label }
+                    '=' in step -> {
+                        val (label, focalLength) = step.split("=")
+                        val content = boxes[hash(label)]
+                        val index = content.indexOfFirst { it.first == label }
                         if (index >= 0) {
-                            list[index] = label to focal
+                            content[index] = label to focalLength
                         } else {
-                            list.add(label to focal)
+                            content.add(label to focalLength)
                         }
                     }
                 }
-                "After \"$input\":".log()
-                boxes.withIndex()
-                    .filter { it.value.isNotEmpty() }
-                    .forEach { (i, box) ->
-                        val content = box.joinToString { "[${it.first} ${it.second}]" }
-                        "Box $i $content".log()
-                    }
-
-                println()
             }
 
             boxes.withIndex().sumOf { (boxIndex, lenses) ->
-                val lenseSum = lenses.withIndex().sumOf { (indexInBox, entry) ->
-                    (indexInBox + 1) * entry.second
+                val sum = lenses.withIndex().sumOf { (indexInBox, entry) ->
+                    (indexInBox + 1) * entry.second.toInt()
                 }
-                (boxIndex + 1) * lenseSum
+                (boxIndex + 1) * sum
             }
         }
         verify {
-            expect result null
+            expect result 284132
             run test 1 expect 145
         }
     }
 }
 
-private fun hash(it: String) =
-    it.fold(0) { current, c ->
-        (current + c.code) * 17 % 256
-    }
+private fun hash(it: String) = it.fold(0) { current, c -> (current + c.code) * 17 % 256 }
