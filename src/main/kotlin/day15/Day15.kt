4e8d96b9c0342f2ff9656fd4064d9efd2602b4c9
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2023-12-15 06:07:33 +0100

    Day 15 Part 1 done

--- src/main/kotlin/day15/Day15.kt
+++ src/main/kotlin/day15/Day15.kt
@@ -2,17 +2,23 @@ package day15
 
 import common.day
 
-// answer #1:
+// answer #1: 502139
 // answer #2:
 
 fun main() {
     day(n = 15) {
         part1 { input ->
-
+            input.lines.first()
+                .split(",")
+                .sumOf {
+                    it.fold(0L) { current, c ->
+                        (current + c.code) * 17 % 256
+                    }
+                }
         }
         verify {
-            expect result null
-            run test 1 expect Unit
+            expect result 502139
+            run test 1 expect 1320L
         }
 
         part2 { input ->
