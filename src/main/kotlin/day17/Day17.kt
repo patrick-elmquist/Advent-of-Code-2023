b4a2d649f7ef9466c2f4e63363015a00bd5f5b6a
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2023-12-17 15:03:19 +0100

    Day 17 working

--- src/main/kotlin/day17/Day17.kt
+++ src/main/kotlin/day17/Day17.kt
@@ -4,10 +4,16 @@ import common.day
 import common.pointCharMap
 import common.util.*
 import java.util.*
-import kotlin.math.abs
 
-// answer #1:
+// answer #1: 755
 // answer #2:
+
+data class Node(
+    val point: Point,
+    val direction: Direction,
+    val steps: Int,
+    val heatLoss: Int,
+)
 
 fun main() {
     day(n = 17) {
@@ -18,251 +24,144 @@ fun main() {
             val start = Point(x = 0, y = 0)
             val end = Point(x = lines.first().lastIndex, y = lines.lastIndex).log()
 
-            data class Node(
-                val point: Point,
-                val direction: Direction,
-                val steps: Int,
+            val visited = mutableSetOf<Triple<Point, Direction, Int>>()
+            val queue = PriorityQueue<Node>(compareBy { it.heatLoss })
+            queue.add(
+                Node(
+                    point = start.rightNeighbour,
+                    direction = Direction.Right,
+                    steps = 1,
+                    heatLoss = map.getValue(start.rightNeighbour),
+                ),
+            )
+            queue.add(
+                Node(
+                    point = start.belowNeighbour,
+                    direction = Direction.Down,
+                    steps = 1,
+                    heatLoss = map.getValue(start.belowNeighbour),
+                ),
             )
 
-            val visited = mutableSetOf(Triple(start, Direction.Right, 0))
-            val distance = mutableMapOf<Point, Int>().withDefault { Int.MAX_VALUE }
-            distance[start] = 0
-            distance[start.rightNeighbour] = map.getValue(start.rightNeighbour)
-            distance[start.belowNeighbour] = map.getValue(start.belowNeighbour)
-            val nodes = PriorityQueue<Node>(compareBy { distance})
-            val prev = mutableMapOf<Point, Point>()
-            prev[start.rightNeighbour] = start
-            prev[start.belowNeighbour] = start
-            nodes.apply {
-                add(Node(start.rightNeighbour, Direction.Right, 1))
-                add(Node(start.belowNeighbour, Direction.Down, 1))
-            }
+            while (queue.isNotEmpty()) {
+                val node = queue.poll()
 
-            while (nodes.isNotEmpty()) {
-                val node = nodes.minBy { distance.getValue(it.point) }
-                nodes.remove(node)
-                println("PROCESSING: ${node.point} dir:${node.direction}")
-                val loss = distance.getValue(node.point)
+                if (node.point == end) {
+                    return@part1 node.heatLoss
+                }
 
-                visited += Triple(node.point, node.direction, node.steps)
+                val key = Triple(node.point, node.direction, node.steps)
+                if (key in visited) {
+                    continue
+                }
 
-                val potentialSteps = buildList {
-                    if (node.steps < 3) {
-                        "adding forward ${node.point.neighborInDirection(node.direction)} ${node.direction} ${node.steps}".log()
-                        add(
+                visited += key
+                if (node.steps < 3) {
+                    val nextPoint = node.point.neighborInDirection(node.direction)
+                    if (nextPoint in map) {
+                        queue.add(
                             Node(
-                                point = node.point.neighborInDirection(node.direction),
+                                point = nextPoint,
                                 direction = node.direction,
                                 steps = node.steps + 1,
+                                heatLoss = node.heatLoss + map.getValue(nextPoint)
                             )
                         )
                     }
-
-                    val leftDir = node.direction.nextCCW
-                    val leftPoint = node.point.neighborInDirection(leftDir)
-                    add(
-                        Node(
-                            point = leftPoint,
-                            direction = leftDir,
-                            steps = 1,
-                        )
-                    )
-
-                    val rightDir = node.direction.nextCW
-                    val rightPoint = node.point.neighborInDirection(rightDir)
-                    add(
-                        Node(
-                            point = rightPoint,
-                            direction = rightDir,
-                            steps = 1,
-                        )
-                    )
                 }
-
-                potentialSteps.forEach { next ->
-                    when {
-                        next.point !in map -> {}
-                        Triple(next.point, next.direction, next.steps) !in visited -> {
-                            val newLoss = loss + map.getValue(next.point)
-                            val existingLoss = distance.getValue(next.point)
-                            if (newLoss < existingLoss) {
-                                "queuing $next with loss $newLoss".log()
-                                distance[next.point] = newLoss
-                                nodes.add(next)
-                                prev[next.point] = node.point
-                            } else {
-                                "ignoring $next with newloss $newLoss >= $existingLoss".log()
-                            }
-                        }
-
-                        else -> {
-                            "${next.point} was in visited"
-                        }
+                listOf(node.direction.nextCW, node.direction.nextCCW).forEach { newDir ->
+                    val newPoint = node.point.neighborInDirection(newDir)
+                    if (newPoint in map) {
+                        queue.add(
+                            Node(
+                                point = newPoint,
+                                direction = newDir,
+                                steps = 1,
+                                heatLoss = node.heatLoss + map.getValue(newPoint)
+                            )
+                        )
                     }
                 }
             }
-            distance[end].log("end ")
-            var current = end
-            val path = mutableListOf<Point>()
-            while (current != start) {
-                path += current
-                current = prev.getValue(current)
-            }
-            path.reversed().log("path:")
-            printMap(map, path.toSet())
-            println()
-            val minMaxX = map.minOf { it.key.x }..map.maxOf { it.key.x }
-            val minMaxY = map.minOf { it.key.y }..map.maxOf { it.key.y }
-            for (y in minMaxY) {
-                for (x in minMaxX) {
-                    val p = Point(x, y)
-                    val distance = distance.getValue(p)
-                    if (distance < 10) {
-                        print(" $distance ")
-                    } else {
-                        print("$distance ")
-                    }
-                }
-                println()
-            }
-
-            distance[end]
         }
         verify {
-            expect result null
+            expect result 755
             run test 2 expect 7
             run test 3 expect 5
             run test 1 expect 102
         }
 
         part2 { input ->
+            val lines = input.lines
+            val map = input.pointCharMap.mapValues { it.value.digitToInt() }
 
-        }
-        verify {
-            expect result null
-            run test 1 expect Unit
-        }
-    }
-}
+            val start = Point(x = 0, y = 0)
+            val end = Point(x = lines.first().lastIndex, y = lines.lastIndex).log()
 
-private fun initialTry(end: Point, map: Map<Point, Int>): Int {
-    data class Node(
-        val p: Point,
-        val direction: Direction,
-        val stepsInDirection: Int,
-        val heatLost: Int,
-        val visitedNodes: Set<Point>,
-        val path: List<Pair<Point, Direction>> = listOf(p to direction),
-    )
+            val visited = mutableSetOf<Triple<Point, Direction, Int>>()
+            val queue = PriorityQueue<Node>(compareBy { it.heatLoss })
+            queue.add(
+                Node(
+                    point = start.rightNeighbour,
+                    direction = Direction.Right,
+                    steps = 1,
+                    heatLoss = map.getValue(start.rightNeighbour),
+                ),
+            )
+            queue.add(
+                Node(
+                    point = start.belowNeighbour,
+                    direction = Direction.Down,
+                    steps = 1,
+                    heatLoss = map.getValue(start.belowNeighbour),
+                ),
+            )
 
-    val ends = mutableListOf<Int>()
-    val visited = mutableMapOf<Point, Int>()
-    val nodes = PriorityQueue<Node> { o1, o2 ->
-        o1.heatLost.compareTo(o2.heatLost)
-    }
-    nodes.add(
-        Node(
-            p = Point(1, 0),
-            direction = Direction.Right,
-            heatLost = 0,
-            stepsInDirection = 1,
-            visitedNodes = emptySet(),
-        )
-    )
+            while (queue.isNotEmpty()) {
+                val node = queue.poll()
 
-    while (nodes.isNotEmpty()) {
-        val node = nodes.remove()
-//                nodes.size.log("node ${node.p} dir:${node.direction} stepsInDir:${node.stepsInDirection} loss:${node.heatLost} size:")
-        nodes.size.log("size: ")
+                if (node.point == end) {
+                    return@part2 node.heatLoss
+                }
 
-        val loss = map.getValue(node.p)
-        val heatLost = node.heatLost + loss
-        if (node.p == end) {
-//            printMap(map, node.path)
-            println()
-            println("end $heatLost")
-            ends += heatLost
-            break
-        }
+                val key = Triple(node.point, node.direction, node.steps)
+                if (key in visited) {
+                    continue
+                }
 
-        visited[node.p] = heatLost
-
-        if (node.stepsInDirection < 3) {
-            val nextPoint = node.p.neighborInDirection(node.direction)
-            if (nextPoint in map) {
-                val allowed = nextPoint !in node.visitedNodes
-                if (allowed) {
-                    nodes.add(
-                        Node(
-                            p = nextPoint,
-                            direction = node.direction,
-                            stepsInDirection = node.stepsInDirection + 1,
-                            heatLost = heatLost,
-                            path = node.path + (nextPoint to node.direction),
-                            visitedNodes = node.visitedNodes + node.p,
+                visited += key
+                if (node.steps < 3) {
+                    val nextPoint = node.point.neighborInDirection(node.direction)
+                    if (nextPoint in map) {
+                        queue.add(
+                            Node(
+                                point = nextPoint,
+                                direction = node.direction,
+                                steps = node.steps + 1,
+                                heatLoss = node.heatLoss + map.getValue(nextPoint)
+                            )
                         )
-                    )
+                    }
+                }
+                listOf(node.direction.nextCW, node.direction.nextCCW).forEach { newDir ->
+                    val newPoint = node.point.neighborInDirection(newDir)
+                    if (newPoint in map) {
+                        queue.add(
+                            Node(
+                                point = newPoint,
+                                direction = newDir,
+                                steps = 1,
+                                heatLoss = node.heatLoss + map.getValue(newPoint)
+                            )
+                        )
+                    }
                 }
             }
         }
-
-        // add left
-        val leftDirection = node.direction.nextCCW
-        val leftPoint = node.p.neighborInDirection(leftDirection)
-        if (leftPoint in map) {
-            val allowed = leftPoint !in node.visitedNodes
-            if (allowed) {
-                nodes.add(
-                    Node(
-                        p = leftPoint,
-                        direction = leftDirection,
-                        stepsInDirection = 1,
-                        heatLost = heatLost,
-                        path = node.path + (leftPoint to leftDirection),
-                        visitedNodes = node.visitedNodes + node.p,
-                    )
-                )
-            }
+        verify {
+            expect result null
+            run test 1 expect 94
+            run test 4 expect 71
         }
-
-        // add right
-        val rightDirection = node.direction.nextCW
-        val rightPoint = node.p.neighborInDirection(rightDirection)
-        if (rightPoint in map) {
-            val allowed = rightPoint !in node.visitedNodes
-            if (allowed) {
-                nodes.add(
-                    Node(
-                        p = rightPoint,
-                        direction = rightDirection,
-                        stepsInDirection = 1,
-                        heatLost = heatLost,
-                        path = node.path + (rightPoint to rightDirection),
-                        visitedNodes = node.visitedNodes + node.p,
-                    )
-                )
-            }
-        }
-    }
-    return ends.first()
-}
-
-private fun Point.distance(other: Point): Int =
-    abs(other.x - x) + abs(other.y - y)
-
-private fun printMap(map: Map<Point, Int>, path: Set<Point>) {
-    val minMaxX = map.minOf { it.key.x }..map.maxOf { it.key.x }
-    val minMaxY = map.minOf { it.key.y }..map.maxOf { it.key.y }
-
-    for (y in minMaxY) {
-        for (x in minMaxX) {
-            val p = Point(x, y)
-            if (p in path) {
-                print('*')
-            } else {
-                print(map[p])
-            }
-        }
-        println()
     }
 }
