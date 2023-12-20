4071b6ce3b779b57232b6c270c57cc220ae14f37
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2023-12-20 13:56:37 +0100

    Day 20 Part 2 done, hardcoded and horrible but done

--- src/main/kotlin/day20/Day20.kt
+++ src/main/kotlin/day20/Day20.kt
@@ -1,21 +1,32 @@
 package day20
 
+import common.Input
 import common.day
+import common.util.leastCommonMultiple
 
 // answer #1: 731517480
-// answer #2:
+// answer #2: 244178746156661
+
+private const val low = "low"
+private const val high = "high"
+private const val on = "on"
+private const val off = "off"
 
 fun main() {
     day(n = 20) {
         part1 { input ->
-            val map = input.lines.map { line -> line.split(" -> ") }
-                .associate { (module, destinations) -> module to destinations.split(", ") }
-
-            val n = 1000
-            var state = State()
-            repeat(n) {
-                state = pushButton(map, state)
+            val map = parseModuleToDestinationsMap(input)
+            val nameToModule = map.keys.associateBy {
+                when {
+                    it.startsWith('%') -> it.drop(1)
+                    it.startsWith('&') -> it.drop(1)
+                    it == "broadcaster" -> it
+                    else -> error("")
+                }
             }
+
+            var state = State()
+            repeat(1000) { state = pushButton(map, nameToModule, state, start = "broadcaster") }
             state.lows * state.highs
         }
         verify {
@@ -25,47 +36,58 @@ fun main() {
         }
 
         part2 { input ->
+            val map = parseModuleToDestinationsMap(input)
+            val loopStartModules = map.getValue("broadcaster")
+            val nameToModule = map.keys.associateBy {
+                when {
+                    it.startsWith('%') -> it.drop(1)
+                    it.startsWith('&') -> it.drop(1)
+                    it == "broadcaster" -> it
+                    else -> error("")
+                }
+            }
 
+            loopStartModules.map { start ->
+                var state = State()
+                var counter = 0L
+                while (!state.done) {
+                    state = pushButton(map, nameToModule, state, start = start, endCriteria = "kh" to high)
+                    counter++
+                }
+                counter
+            }.reduce(::leastCommonMultiple)
         }
         verify {
-            expect result null
-            run test 1 expect Unit
+            expect result 244178746156661L
         }
     }
 }
 
-private const val low = "low"
-private const val high = "high"
-private const val on = "on"
-private const val off = "off"
+private fun parseModuleToDestinationsMap(input: Input) = input.lines.map { line -> line.split(" -> ") }
+    .associate { (module, destinations) -> module to destinations.split(", ") }
 
-private fun pushButton(map: Map<String, List<String>>, state: State): State {
-    val nameToModule = map.keys.associateBy {
-        when {
-            it.startsWith('%') -> it.drop(1)
-            it.startsWith('&') -> it.drop(1)
-            it == "broadcaster" -> it
-            else -> error("")
-        }
-    }
-
+private fun pushButton(
+    map: Map<String, List<String>>,
+    nameToModule: Map<String, String>,
+    state: State,
+    start: String,
+    endCriteria: Pair<String, String>? = null,
+): State {
     val queue = ArrayDeque<Triple<String, String, String>>()
-    queue.add(Triple("broadcaster", low, ""))
-
-    val memory = state.memory
-    val states = state.states
+    queue.add(Triple(start, low, ""))
 
     var lows = 1L
     var highs = 0L
 
     fun emit(destinations: List<String>, pulse: String, module: String): List<Triple<String, String, String>> {
-        return destinations.map { Triple(it, pulse, module) }.print(module).also {
-            if (pulse == low) {
-                lows += it.size
-            } else {
-                highs += it.size
+        return destinations.map { Triple(it, pulse, module) }
+            .also {
+                if (pulse == low) {
+                    lows += it.size
+                } else {
+                    highs += it.size
+                }
             }
-        }
     }
     while (queue.isNotEmpty()) {
         val (name, pulse, source) = queue.removeFirst()
@@ -73,10 +95,15 @@ private fun pushButton(map: Map<String, List<String>>, state: State): State {
         val module = nameToModule.getOrDefault(name, name)
         val destinations = map.getOrDefault(module, emptyList())
 
+        if (name == endCriteria?.first && pulse == endCriteria.second) {
+            return state.copy(
+                lows = state.lows + lows,
+                highs = state.highs + highs,
+                done = true,
+            )
+        }
         when {
-            module == "output" -> {
-//                println("output")
-            }
+            module !in map -> {}
 
             module == "broadcaster" -> {
                 queue += emit(destinations, pulse, module)
@@ -84,14 +111,14 @@ private fun pushButton(map: Map<String, List<String>>, state: State): State {
 
             module.startsWith('%') -> {
                 if (pulse == low) {
-                    when (states.getOrPut(module) { off }) {
+                    when (state.states.getOrPut(module) { off }) {
                         on -> {
-                            states[module] = off
+                            state.states[module] = off
                             queue += emit(destinations, low, module)
                         }
 
                         off -> {
-                            states[module] = on
+                            state.states[module] = on
                             queue += emit(destinations, high, module)
                         }
 
@@ -101,7 +128,7 @@ private fun pushButton(map: Map<String, List<String>>, state: State): State {
             }
 
             module.startsWith('&') -> {
-                val memo = memory.getOrPut(module) {
+                val memo = state.memory.getOrPut(module) {
                     map.filter { (_, dest) -> dest.contains(name) }
                         .keys
                         .associateWith { low }
@@ -117,12 +144,9 @@ private fun pushButton(map: Map<String, List<String>>, state: State): State {
         }
     }
 
-    println()
-    return State(
+    return state.copy(
         lows = state.lows + lows,
         highs = state.highs + highs,
-        memory = memory,
-        states = states,
     )
 }
 
@@ -131,10 +155,6 @@ private data class State(
     val highs: Long = 0L,
     val memory: MutableMap<String, MutableMap<String, String>> = mutableMapOf(),
     val states: MutableMap<String, String> = mutableMapOf(),
+    val done: Boolean = false,
+    val counter: Long = 0L,
 )
-private fun List<Triple<String, String, String>>.print(module: String): List<Triple<String, String, String>> {
-    forEach { (dest, pulse, _) ->
-        println("$module -$pulse-> $dest")
-    }
-    return this
-}
