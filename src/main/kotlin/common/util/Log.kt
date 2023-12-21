b4ff0f2d8535294216e3b816fec4b978f4922a44
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2023-12-21 16:24:08 +0100

    Not working...

--- src/main/kotlin/common/util/Log.kt
+++ src/main/kotlin/common/util/Log.kt
@@ -2,6 +2,10 @@
 
 package common.util
 
+import kotlinx.coroutines.flow.merge
+import kotlin.math.max
+import kotlin.math.min
+
 /**
  * Abuse the not operator fun for quick logging
  * If you are reading this, for the love of god, don't use this in production code
@@ -10,6 +14,7 @@ package common.util
 operator fun String.not() = println(this)
 
 var loggingEnabled = true
+var tagSize = 0
 
 inline fun <T> T.log(): T {
     if (!loggingEnabled) return this
@@ -23,5 +28,7 @@ inline fun <T> T.log(msg: () -> Any): T {
 
 inline infix fun <T> T.log(msg: Any): T {
     if (!loggingEnabled) return this
-    return this.also { println("$msg$it") }
+    val tag = msg.toString()
+    tagSize = max(tagSize, tag.length)
+    return this.also { println("${tag.padStart(tagSize, ' ')}\t$it") }
 }
--- src/main/kotlin/day21/Day21.kt
+++ src/main/kotlin/day21/Day21.kt
@@ -1,8 +1,6 @@
 package day21
 
-import common.day
-import common.grid
-import common.pointCharMap
+import common.*
 import common.util.*
 import kotlin.collections.ArrayDeque
 
@@ -14,14 +12,59 @@ fun main() {
         part1 { input ->
             val map = input.grid
             val start = map.entries.first { it.value == 'S' }.key
-            findEndpoints(start, steps = 64, map)
+            map.findEndPoints(start, steps = 64)
         }
         verify {
-            expect result 3572
+            expect result 3572L
         }
 
-        part2 {
+        part2 { input ->
+            val map = input.grid
+            val start = map.entries.first { it.value == 'S' }.key
+            val size = input.bounds.first log "size"
+            val steps = 26501365L
 
+            assert(input.bounds.first == input.bounds.second)
+            assert(start.x == start.y)
+            assert(start.x == size / 2)
+            assert((steps % size) == size / 2L)
+
+            val width = (steps / size - 1) log "width"
+
+            val odd = (width / 2 * 2 + 1).let { it * it } log "odd"
+            val even = ((width + 1) / 2 * 2).let { it * it } log "even"
+
+            // above is correct
+
+            val oddPoints = map.findEndPoints(start, size * 2 + 1) log "odd points"
+
+            val evenPoints = map.findEndPoints(start, size * 2) log "even points"
+
+            val cornerSteps = size - 1
+            val topCorner = map.findEndPoints(start.copy(y = size - 1), cornerSteps)
+            val rightCorner = map.findEndPoints(start.copy(x = 0), cornerSteps)
+            val bottomCorner = map.findEndPoints(start.copy(y = 0), cornerSteps)
+            val leftCorner = map.findEndPoints(start.copy(x = size - 1), cornerSteps)
+
+            val smallSteps = size / 2 - 1
+            val smallTopRight = map.findEndPoints(Point(0, size - 1), smallSteps)
+            val smallTopLeft = map.findEndPoints(Point(size - 1, size - 1), smallSteps)
+            val smallBottomRight = map.findEndPoints(Point(0, 0), smallSteps)
+            val smallBottomLeft = map.findEndPoints(Point(size - 1, 0), smallSteps)
+
+            val largeSteps = size * 3 / 2 - 1
+            val largeTopRight = map.findEndPoints(Point(0, size - 1), largeSteps)
+            val largeTopLeft = map.findEndPoints(Point(size - 1, size - 1), largeSteps)
+            val largeBottomRight = map.findEndPoints(Point(0, 0), largeSteps)
+            val largeBottomLeft = map.findEndPoints(Point(size - 1, 0), largeSteps)
+
+            odd * oddPoints +
+                    even * evenPoints +
+                    topCorner + rightCorner + bottomCorner + leftCorner +
+                    (width + 1) * (smallTopRight + smallTopLeft + smallBottomRight + smallBottomLeft) +
+                    width * (largeTopRight + largeTopLeft + largeBottomRight + largeBottomLeft)
+            // not 594600652054873, too low
+            // not 594606492802729, too low
         }
         verify {
             expect result null
@@ -29,11 +72,10 @@ fun main() {
     }
 }
 
-private fun findEndpoints(
+private fun Map<Point, Char>.findEndPoints(
     start: Point,
-    steps: Int,
-    map: Map<Point, Char>
-): Int {
+    steps: Int
+): Long {
     val visited = mutableSetOf(start)
     val possibleEndPoints = mutableSetOf<Point>()
 
@@ -50,7 +92,7 @@ private fun findEndpoints(
         if (nextStepsLeft >= 0) {
             point.neighbors()
                 .filter { it !in visited }
-                .filter { map[it] == '.' }
+                .filter { this[it] == '.' }
                 .forEach { next ->
                     visited += next
                     queue += next to nextStepsLeft
@@ -58,5 +100,5 @@ private fun findEndpoints(
         }
     }
 
-    return possibleEndPoints.size
+    return possibleEndPoints.size.toLong()
 }
