d367a70a614ce0348e2d37915553366aa4746c07
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2023-12-24 00:33:58 +0100

    Day 23 Part 2 done

--- src/main/kotlin/day23/Day23.kt
+++ src/main/kotlin/day23/Day23.kt
@@ -6,216 +6,105 @@ import common.grid
 import common.util.*
 
 // answer #1: 2334
-// answer #2:
+// answer #2: 6422
 
 fun main() {
     day(n = 23) {
         part1 { input ->
             val grid = input.grid
             val (_, height) = input.bounds
-            val start = grid.entries.filter { it.key.y == 0 }.single { it.value == '.' }
-            val end = grid.entries.filter { it.key.y == height - 1 }.single { it.value == '.' }
-
-            val nodeWithNeighbors = topologicalSort(grid, start.key)
-
-            fun rec(node: Point, steps: Int, visited: Set<Point>): Int {
-                if (node == end.key) {
-                    return steps
+            val start = grid.entries.first { it.key.y == 0 && it.value == '.' }.key
+            val end = grid.entries.first { it.key.y == height - 1 && it.value == '.' }.key
+            findMax(
+                current = start,
+                end = end,
+                getNeighbours = { node ->
+                    when (grid[node]) {
+                        '>' -> listOf(node.rightNeighbour to 1)
+                        '<' -> listOf(node.leftNeighbour to 1)
+                        '^' -> listOf(node.aboveNeighbour to 1)
+                        'v' -> listOf(node.belowNeighbour to 1)
+                        else -> grid.getNeighbours(node).map { it to 1 }.toList()
+                    }
                 }
-
-                val paths = nodeWithNeighbors.getValue(node)
-                    .filter { it !in visited }
-
-                if (paths.isEmpty()) {
-                    return Int.MIN_VALUE
-                }
-
-                return paths.maxOf {
-                    rec(it, steps + 1, visited + node)
-                }
-            }
-
-            rec(start.key, steps = 0, visited = emptySet())
+            )
         }
         verify {
             expect result 2334
-            run test 1 expect 1 // fail to avoid running 1 94
+            run test 1 expect 94
         }
 
         part2 { input ->
             val grid = input.grid
             val (_, height) = input.bounds
-            val start = grid.entries.filter { it.key.y == 0 }
-                .single { it.value == '.' }
-            val end = grid.entries.filter { it.key.y == height - 1 }.single { it.value == '.' }
+            val start = grid.entries.first { it.key.y == 0 && it.value == '.' }.key
+            val end = grid.entries.first { it.key.y == height - 1 && it.value == '.' }.key
 
-            fun calculateSegment(start: Point, last: Point): List<Point> {
-                val queue = ArrayDeque<Point>()
-                queue += start
-                val segment = mutableListOf<Point>()
-                var prev = last
-                while (queue.isNotEmpty()) {
-                    val point = queue.removeFirst()
+            val junctions = mutableMapOf<Point, MutableList<Pair<Point, Int>>>(
+                start to mutableListOf(),
+                end to mutableListOf(),
+            )
 
-                    segment += point
+            grid.filter { (_, c) -> c == '.' }
+                .filter { (point, _) -> grid.getNeighbours(point).size > 2 }
+                .forEach { (point, _) -> junctions[point] = mutableListOf() }
 
-                    val neighbors = point.neighbors()
-                        .filter { it in grid && grid.getValue(it) != '#' }
-                        .toList()
+            junctions.keys.forEach { junction ->
+                var current = setOf(junction)
+                val visited = mutableSetOf(junction)
+                var distance = 0
 
-                    if (neighbors.count { grid[it] in setOf('<', '>', 'v', '^') } > 2) {
-                        "$point breaking due to $neighbors".log()
-                        break
-                    } else {
-                        "$point $neighbors".log()
-                        neighbors.filter { it != prev }.firstOrNull()?.let {
-                            queue += it
-                        }
-                        prev = point
-                    }
-                }
-                return segment
-            }
-
-            fun calculateSegments(start: Point, end: Point): Map<Point, List<Point>> {
-                val segments = mutableMapOf<Point, List<Point>>()
-                val queue = ArrayDeque<Pair<Point, Point>>()
-                queue += start to start.aboveNeighbour
-                val visited = mutableSetOf<Point>()
-
-                while (queue.isNotEmpty()) {
-                    val (node, last) = queue.removeFirst()
-                    "CHECK $node ($last)".log()
-
-                    if (node in visited) continue
-                    if (node == end) continue
-
-                    val segment = calculateSegment(node, last)
-                    segments += node to (listOf(last) + segment)
-                    visited += segment
-
-                    queue += segment.last().neighbors()
-                        .filter { it in grid && grid[it] != '#' }
+                while (current.isNotEmpty()) {
+                    distance++
+                    val updatedSet = mutableSetOf<Point>()
+                    current.flatMap { grid.getNeighbours(it) }
                         .filter { it !in visited }
-                        .map { it to segment.last() }
-
-                    queue log "queue after $node"
-                    segments log "segments after $node"
-                    println()
-                }
-
-                return segments
-            }
-
-            grid
-                .filter { it.value != '#' }
-                .count {
-                    it.key.neighbors()
-                        .filter { it in grid && grid.getValue(it) != '#' }
-                        .count()
-                        .apply {
-                            if (this > 2) {
-                                "n: ${it.key} = $this".log()
+                        .forEach { n ->
+                            if (n in junctions) {
+                                junctions.getValue(junction).add(n to distance)
+                            } else {
+                                updatedSet += n
+                                visited += n
                             }
-                        } > 2
-                } log "n > 2:"
-            println()
-            "segments".log()
-            val segments = calculateSegments(start.key, end.key)
-                .also {
-                    it.forEach { (start, segment) ->
-                        "start:$start (${segment.size})".log()
-                        segment.forEach {
-                            it.log()
                         }
-                        println()
-                    }
-                    it.size log "size:"
-                }
-
-            val startSegment = segments[start.key] log "start segment"
-            val segmentLen = segments.mapValues { it.value.size }
-            val segmentStartEnd = segments.mapValues { it.value.last() }.toMap()
-
-            fun rec(segment: List<Point>, visited: Set<List<Point>>): List<Point> {
-                if (segment.last() == end.key) return segment
-
-                val visit = visited + setOf(segment)
-                return segment + (segments.values
-                    .filter { it.first() == segment.last() }
-                    .filter { it !in visit}
-                    .map {
-                        val rec = rec(it, visit)
-                        rec.size log "check ${segment.first()}"
-                        rec
-                    }
-                    .maxByOrNull { it.size }.also { "returning".log() } ?: emptyList())
-            }
-
-//            val points = segments.entries
-//                .filter { it.value.first() == segments.getValue(start.key).last() }
-//                .onEach { it.value.size log "size" }
-//                .first().value
-            val points = rec(segments.getValue(start.key), emptySet()) log "rec"
-            points.size log "rec size"
-
-            grid.print { point, c ->
-                if (point in points) {
-                    'O'
-                } else {
-                    c
+                    current = updatedSet
                 }
             }
 
-            TODO()
+            findMax(current = start, end = end, getNeighbours = junctions::getValue)
         }
         verify {
-            expect result null
+            expect result 6422
             run test 1 expect 154
         }
     }
 }
 
-private fun getNeighbors(point: Point): List<Pair<Point, Direction>> {
-    return listOf(
-        point.aboveNeighbour to Direction.Up,
-        point.belowNeighbour to Direction.Down,
-        point.leftNeighbour to Direction.Left,
-        point.rightNeighbour to Direction.Right,
-    )
-}
+private fun findMax(
+    current: Point,
+    end: Point,
+    visited: MutableSet<Point> = mutableSetOf(),
+    distance: Int = 0,
+    getNeighbours: (Point) -> List<Pair<Point, Int>>,
+): Int {
+    if (current == end) return distance
 
-private fun topologicalSort(
-    grid: Map<Point, Char>,
-    start: Point,
-    ignoreSlopes: Boolean = false
-): Map<Point, List<Point>> {
-    val nodeWithNeighbors = mutableMapOf<Point, List<Point>>()
-    val visited = mutableSetOf<Point>()
-    fun visit(node: Point) {
-        if (node !in visited) {
-            visited += node
-
-            val potentialNextSteps = if (ignoreSlopes) {
-                node.neighbors().toList()
-            } else {
-                when (grid[node]) {
-                    '>' -> listOf(node.rightNeighbour)
-                    '<' -> listOf(node.leftNeighbour)
-                    '^' -> listOf(node.aboveNeighbour)
-                    'v' -> listOf(node.belowNeighbour)
-                    '.' -> node.neighbors().toList()
-                    else -> error("")
-                }
-            }
-
-            val neighbors = potentialNextSteps
-                .filter { it in grid && grid[it] != '#' }
-
-            neighbors.onEach { visit(it) }
-            nodeWithNeighbors[node] = neighbors
+    visited += current
+    val max = getNeighbours(current)
+        .filter { (neighbour, _) -> neighbour !in visited }
+        .maxOfOrNull { (neighbour, weight) ->
+            findMax(
+                current = neighbour,
+                end = end,
+                visited = visited,
+                distance = distance + weight,
+                getNeighbours = getNeighbours,
+            )
         }
-    }
-    visit(start)
-    return nodeWithNeighbors
+    visited -= current
+
+    return max ?: 0
 }
+
+private fun Map<Point, Char>.getNeighbours(point: Point): List<Point> =
+    point.neighbors().filter { it in this && getValue(it) in ".<>^v" }.toList()
